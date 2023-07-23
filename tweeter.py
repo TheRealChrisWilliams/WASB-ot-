287c42fb9ca987fdb67a22e6de273d3f198b9ebb
Chris Williams <41800074+TheRealChrisWilliams@users.noreply.github.com>
2023-07-24 00:42:50 +0530

    Update tweeter.py

--- tweeter.py
+++ tweeter.py
@@ -1,43 +1,26 @@
 import tweepy
-import time
 from scraper import tips_and_resources
+import time
+from decouple import config
 
+# Your Twitter API credentials
+consumer_key = config("YOUR_CONSUMER_KEY")
+consumer_secret = config("YOUR_CONSUMER_SECRET")
+access_token = config("YOUR_ACCESS_TOKEN")
+access_token_secret = config("YOUR_ACCESS_TOKEN_SECRET")
 
-def tweeting():
+# Create a Twitter client
+client = tweepy.Client(
+    consumer_key=consumer_key,
+    consumer_secret=consumer_secret,
+    access_token=access_token,
+    access_token_secret=access_token_secret,
+)
 
-    consumer_key = ""
-    consumer_secret = ""
-    access_token = ""
-    access_token_secret = ""
+for item in tips_and_resources:
 
-    # Authenticate with Twitter API
-    auth = tweepy.OAuth1UserHandler(consumer_key, consumer_secret, access_token, access_token_secret)
-    api = tweepy.API(auth)
+    tweet_text = item["title"] + "\n" + item["link"] + "\n#gamedev #indiegames #gamingnews #gamingindustry #gamebot" + " #" + item['title'].split(" ", 1)[0].lower()
 
-    tweeted = set()  # Create a set to store previously tweeted articles
-
-    for tweet in tweepy.Cursor(api.user_timeline).items():
-        tweeted.add(tweet.text.split("\n", 1)[0])
-
-    print(tweeted)
-
-    while True:
-        for item in tips_and_resources:
-            tweet_exists = False
-            # Check if the current tweet has already been posted
-            if item['title'].strip() in tweeted:
-                continue
-            
-            if tweet_exists:
-                continue
-            
-            if not tweet_exists:  # If tweet hasn't been tweeted, tweet it
-                try:
-                    api.update_status(item["title"] + "\n" + item["link"] + "\n#gamedev #indiegames #gamingnews #gamingindustry #gamebot" + " #" + item['title'].split(" ", 1)[0].lower())
-                    time.sleep(20)
-                except tweepy.errors.TweepyException as e:  # Exception handling
-                    if e == 187:
-                        print("Duplicate tweet.\n")
-                        continue
-                    else:
-                        continue
+    response = client.create_tweet(text=tweet_text)
+    print(response)
+    time.sleep(20)
